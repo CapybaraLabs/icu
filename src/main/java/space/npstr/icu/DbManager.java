ebd775cfa87e4c743b4d2b1ecdb6b51ad21a7ca3
Napster <napster@npstr.space>
2018-08-05 19:35:23 +0200

    Place more constants in database configuration strings

--- src/main/java/space/npstr/icu/DbManager.java
+++ src/main/java/space/npstr/icu/DbManager.java
@@ -19,6 +19,8 @@ package space.npstr.icu;
 
 import net.ttddyy.dsproxy.listener.logging.SLF4JLogLevel;
 import net.ttddyy.dsproxy.support.ProxyDataSourceBuilder;
+import org.hibernate.cfg.Environment;
+import org.hibernate.tool.schema.Action;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import space.npstr.icu.info.AppInfo;
@@ -70,30 +72,29 @@ public class DbManager {
     private static DatabaseConnection initDefaultDbConn() {
         try {
             return new DatabaseConnection.Builder("postgres", Config.C.jdbcUrl)
-                    .setDialect("org.hibernate.dialect.PostgreSQL95Dialect")
+                    .setDialect(org.hibernate.dialect.PostgreSQL95Dialect.class.getName())
                     .addEntityPackage("space.npstr.icu.db.entities")
                     .setAppName("icu_" + AppInfo.getAppInfo().getVersionBuild())
                     .setProxyDataSourceBuilder(new ProxyDataSourceBuilder()
                             .logSlowQueryBySlf4j(10, TimeUnit.SECONDS, SLF4JLogLevel.WARN, "SlowQueryLog")
                             .multiline()
                     )
-                    .setHibernateProperty("hibernate.hbm2ddl.auto", "update")
-                    .setHibernateProperty("hibernate.cache.use_second_level_cache", "true")
-                    .setHibernateProperty("hibernate.cache.use_query_cache", "true")
+                    .setHibernateProperty(Environment.HBM2DDL_AUTO, Action.UPDATE.name().toLowerCase())
+                    .setHibernateProperty(Environment.USE_SECOND_LEVEL_CACHE, true)
+                    .setHibernateProperty(Environment.USE_QUERY_CACHE, true)
                     .setHibernateProperty("net.sf.ehcache.configurationResourceName", "/ehcache.xml")
                     .setHibernateProperty("hibernate.cache.provider_configuration_file_resource_path", "ehcache.xml")
                     .setHibernateProperty("hibernate.cache.region.factory_class", "org.hibernate.cache.ehcache.EhCacheRegionFactory")
 
                     //hide some exception spam on start, as postgres does not support CLOBs
                     // https://stackoverflow.com/questions/43905119/postgres-error-method-org-postgresql-jdbc-pgconnection-createclob-is-not-imple
-                    .setHibernateProperty("hibernate.jdbc.lob.non_contextual_creation", "true")
+                    .setHibernateProperty(Environment.NON_CONTEXTUAL_LOB_CREATION, true)
 
                     // these may improve performance on slow machines / big databases
                     // see https://stackoverflow.com/questions/10075081/hibernate-slow-to-acquire-postgres-connection
                     // and https://stackoverflow.com/questions/14445838/hibernate-startup-very-slow
-                    .setHibernateProperty("hibernate.jdbc.use_get_generated_keys", "true")
-                    .setHibernateProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect")
-                    .setHibernateProperty("hibernate.temp.use_jdbc_metadata_defaults", "false")
+                    .setHibernateProperty(Environment.USE_GET_GENERATED_KEYS, true)
+                    .setHibernateProperty("hibernate.temp.use_jdbc_metadata_defaults", false)
                     .build();
         } catch (Exception e) {
             String message = "Failed to set up database";
